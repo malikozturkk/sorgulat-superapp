244f8e3bb664460a9af4410c555564fb9d713269
Malik Öztürk <malikozturk975@gmail.com>
2025-02-24 16:44:58 +0300

    feat: add fullscreen time display

--- src/app/saat-kac/page.tsx
+++ src/app/saat-kac/page.tsx
@@ -1,42 +1,47 @@
 import { getRequest } from "@/utils/api";
 import Link from "next/link";
-import { TimezoneData } from "../api/timezones/data";
+import { TimeData, TimezoneData } from "../api/timezones/data";
+import LiveClock from "@/components/Timezone/LiveClock";
 
 export default async function Home() {
     const getCitiesTimezones = await getRequest("/api/timezones/cities")
     const getCountriesTimezones = await getRequest("/api/timezones/countries")
     const randomItemClass = "bg-white hover:bg-primary text-black hover:text-white text-black font-extrabold px-3 text-2xl md:text-5xl"
-    if (!getCitiesTimezones || !getCountriesTimezones) {
+    const getTurkeyData: TimeData = await getRequest(`/api/timezones/turkiye`);
+    if (!getCitiesTimezones || !getCountriesTimezones || !getTurkeyData) {
         return (
             <div>veri alınamadı</div>
         )
     }
     return (
-        <div className="flex justify-center bg-[#333] py-4">
-            <div style={{ width: "75vw" }} className="flex flex-col items-center gap-4">
-                <div className="flex gap-6 flex-wrap items-center">
-                    {getCitiesTimezones.map((timezone: TimezoneData) => (
-                        <Link
-                            key={`/saat-kac/${timezone.name}`}
-                            href={`/saat-kac/${timezone?.slug}/`}
-                            style={{ lineHeight: timezone.selected ? "1.375" : "1.5rem" }}
-                            className={`${timezone.selected ? randomItemClass : "text-lg font-semibold px-1 text-white hover:bg-primary"} duration-300`}
-                        >
-                            {timezone?.name}
-                        </Link>
-                    ))}
-                </div>
-                <div className="flex gap-6 flex-wrap items-center">
-                    {getCountriesTimezones.map((timezone: TimezoneData, index: number) => (
-                        <Link
-                            key={`/saat-kac/${timezone.name}`}
-                            href={`/saat-kac/${timezone?.slug}/`}
-                            style={{ lineHeight: timezone.selected ? "1.375" : "1.5rem" }}
-                            className={timezone.selected ? randomItemClass : "text-lg font-semibold px-1 text-white hover:bg-primary"}
-                        >
-                            {timezone?.name}
-                        </Link>
-                    ))}
+        <div className="flex flex-col items-center gap-5 md:gap-10">
+            <LiveClock initialTime={getTurkeyData} />
+            <div className="flex justify-center bg-[#333] py-4 w-full">
+                <div style={{ width: "75vw" }} className="flex flex-col items-center gap-4">
+                    <div className="flex gap-6 flex-wrap items-center">
+                        {getCitiesTimezones.map((timezone: TimezoneData) => (
+                            <Link
+                                key={`/saat-kac/${timezone.name}`}
+                                href={`/saat-kac/${timezone?.slug}/`}
+                                style={{ lineHeight: timezone.selected ? "1.375" : "1.5rem" }}
+                                className={`${timezone.selected ? randomItemClass : "text-lg font-semibold px-1 text-white hover:bg-primary"} duration-300`}
+                            >
+                                {timezone?.name}
+                            </Link>
+                        ))}
+                    </div>
+                    <div className="flex gap-6 flex-wrap items-center">
+                        {getCountriesTimezones.map((timezone: TimezoneData, index: number) => (
+                            <Link
+                                key={`/saat-kac/${timezone.name}`}
+                                href={`/saat-kac/${timezone?.slug}/`}
+                                style={{ lineHeight: timezone.selected ? "1.375" : "1.5rem" }}
+                                className={timezone.selected ? randomItemClass : "text-lg font-semibold px-1 text-white hover:bg-primary"}
+                            >
+                                {timezone?.name}
+                            </Link>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Timezone/LiveClock/index.tsx
+++ src/components/Timezone/LiveClock/index.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { PopulerCities, TimeData, TimezoneData } from '@/app/api/timezones/data';
+import { PopulerCities, TimeData } from '@/app/api/timezones/data';
 import { getRequest } from '@/utils/api';
 import { padZero } from '@/utils/formatter';
 import Link from 'next/link';
@@ -8,6 +8,7 @@ import { useEffect, useState } from 'react';
 
 export default function LiveClock({ initialTime }: { initialTime: TimeData }) {
     const [currentTime, setCurrentTime] = useState<Date>(new Date(initialTime.dateTime));
+    const [fullScreen, setFullScreen] = useState<boolean>(false);
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -37,27 +38,49 @@ export default function LiveClock({ initialTime }: { initialTime: TimeData }) {
     }).format(date);
 
     return (
-        <div className='flex flex-col gap-4'>
-            <div className="flex flex-col items-center justify-center text-xl p-4 border rounded-lg shadow-md mx-4 max-w-7xl sm:mx-6 lg:mx-8">
+        <div className='flex flex-col gap-4 w-full'>
+            <button
+                className={`transition-all duration-300 
+                ${fullScreen
+                        ? "fixed inset-0 w-full h-full flex flex-col items-center justify-center bg-white z-50 p-8"
+                        : "flex flex-col items-center justify-center text-xl p-4 border rounded-lg shadow-md mx-4 max-w-7xl sm:mx-6 lg:mx-8"
+                    }`}
+                onClick={() => setFullScreen(!fullScreen)}
+            >
+                {fullScreen &&
+                    <div className='absolute right-8 top-8 w-9 md:w-12 curspo' onClick={() => setFullScreen(false)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <path
+                                id="SVGRepo_iconCarrier"
+                                fill="#646ecb"
+                                fillRule="evenodd"
+                                d="M5.293 5.293a1 1 0 0 1 1.414 0L12 10.586l5.293-5.293a1 1 0 1 1 1.414 1.414L13.414 12l5.293 5.293a1 1 0 0 1-1.414 1.414L12 13.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L10.586 12 5.293 6.707a1 1 0 0 1 0-1.414Z"
+                                clipRule="evenodd"
+                            ></path>
+                        </svg>
+                    </div>
+                }
                 <div className='w-full'>
                     <h1 className='font-extrabold text-xl md:text-4xl'>{initialTime.timezone.name}, <span className='font-normal'>{initialTime.locationText} saat kaç</span></h1>
                 </div>
-                <time className='font-bold leading-none' style={{ fontSize: "15vw" }}>
+                <time className='font-bold leading-none' style={{ fontSize: fullScreen ? "20vw" : "15vw" }}>
                     {padZero(currentTime.getHours())}:{padZero(currentTime.getMinutes())}:
                     {padZero(currentTime.getSeconds())}
                 </time>
                 <p className='text-4xl w-full text-right'>{formattedDate}</p>
-            </div>
-            <ul className='flex gap-3 justify-end flex-wrap mx-4 max-w-7xl sm:mx-6 lg:mx-8'>
-                {initialTime?.populerCities?.map((city: PopulerCities) => (
-                    <Link href={`/saat-kac/${city.slug}`} className='text-center text-xl' key={city.name}>
-                        <li className={`px-5 py-2 hover:bg-primary hover:text-white ${city.selected ? "bg-primary text-white" : "bg-[#eee]"}`}>
-                            <b>{city.name}</b>
-                            <p>{padZero(city.hour)}:{padZero(city.minute)}</p>
-                        </li>
-                    </Link>
-                ))}
-            </ul>
+            </button>
+            {!fullScreen && (
+                <ul className='flex gap-3 justify-end flex-wrap mx-4 max-w-7xl sm:mx-6 lg:mx-8'>
+                    {initialTime?.populerCities?.map((city: PopulerCities) => (
+                        <Link href={`/saat-kac/${city.slug}`} className='text-center text-xl' key={city.name}>
+                            <li className={`px-5 py-2 hover:bg-primary hover:text-white ${city.selected ? "bg-primary text-white" : "bg-[#eee]"}`}>
+                                <b>{city.name}</b>
+                                <p>{padZero(city.hour)}:{padZero(city.minute)}</p>
+                            </li>
+                        </Link>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 }

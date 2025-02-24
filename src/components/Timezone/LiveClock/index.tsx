f7c67922d4262bf06313fbe6f9831a43129198bc
Malik Öztürk <malikozturk975@gmail.com>
2025-02-24 22:34:03 +0300

    feat: add console logs

--- src/components/Timezone/LiveClock/index.tsx
+++ src/components/Timezone/LiveClock/index.tsx
@@ -73,6 +73,9 @@ export default function LiveClock({ initialTime }: { initialTime: TimeData }) {
                 <ul className='flex gap-3 justify-end flex-wrap mx-4 max-w-7xl sm:mx-6 lg:mx-8'>
                     {initialTime?.populerCities?.map((city: PopulerCities) => {
                         const populerCurrentTime = new Date(city.dateTime)
+                        console.log(city.dateTime, "city.dateTime malik1")
+                        console.log(populerCurrentTime, "populerCurrentTime malik2")
+                        console.log(populerCurrentTime.getHours(), "populerCurrentTime.getHours() malik3")
                         return (
                             <Link href={`/saat-kac/${city.slug}`} className='text-center text-xl' key={city.name}>
                                 <li className={`px-5 py-2 hover:bg-primary hover:text-white ${city.selected ? "bg-primary text-white" : "bg-[#eee]"}`}>

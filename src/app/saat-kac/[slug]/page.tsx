4e19ee96daf63ee3239914bfd1265d38c0c015b5
Malik Öztürk <malikozturk975@gmail.com>
2025-03-04 18:10:03 +0300

    feat: add detail page metadata

--- src/app/saat-kac/[slug]/page.tsx
+++ src/app/saat-kac/[slug]/page.tsx
@@ -2,8 +2,52 @@ import LiveClock from "@/components/Timezone/LiveClock";
 import { DifferenceData, TimeData } from "../types/Timezone.types";
 import { getRequest } from "@/utils/api";
 import TimeDifferenceGraph from "@/components/Timezone/TimeDifferenceGraph";
+import { defaultGenerateMetadata } from "@/app/metadataConfig";
 
 type Params = Promise<{ slug: string }>;
+
+export async function generateMetadata({ params }: { params: Params }) {
+    const { slug } = await params
+    try {
+        if (!slug) {
+            return defaultGenerateMetadata()
+        }
+        const data: TimeData = await getRequest(`/timezones/${slug}`);
+        const name = `${data.timezone.name}'${data.locationText}`
+
+        return {
+            title: `${name} saat kaç? | Sorgulat`,
+            description: `Anlık olarak ${name} saatini öğrenin, popüler şehirlerle saat farkını keşfedin ve güneşin doğuş/batış saatlerini görüntüleyin.`,
+            keywords: `saat kac, ${name} saat kac, populer sehirlerde saat kac, saati sorgula`,
+            authors: [
+                {
+                    name: `${name} Saat Kaç`,
+                    url: `https://www.sorgulat.com/saat-kac/${data.timezone.slug}`,
+                },
+            ],
+            icons: '/favicon.ico',
+            openGraph: {
+                title: `${name} Saat Kaç | Sorgulat`,
+                description: `Anlık olarak ${name} saatini öğrenin, popüler şehirlerle saat farkını keşfedin ve güneşin doğuş/batış saatlerini görüntüleyin.`,
+                url: `https://www.sorgulat.com/saat-kac/${data.timezone.slug}`,
+                images:
+                    '/images/detail-open-graph.png',
+                type: 'website',
+                siteName: 'Sorgulat',
+            },
+            twitter: {
+                card: 'summary_large_image',
+                title: `${name} Saat Kaç | Sorgulat`,
+                description: `Anlık olarak ${name} saatini öğrenin, popüler şehirlerle saat farkını keşfedin ve güneşin doğuş/batış saatlerini görüntüleyin.`,
+                images: '/images/detail-open-graph.png',
+                site: '@Sorgulat',
+            },
+        }
+    } catch (e) {
+        console.error(slug, e, '-> error')
+        return defaultGenerateMetadata()
+    }
+}
 
 export default async function WhatTimeIsIt({ params }: { params: Params }) {
     const { slug } = await params

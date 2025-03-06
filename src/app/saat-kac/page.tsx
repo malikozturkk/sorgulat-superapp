46f18d9e980b207cf774c7cf49f3c8fe3e2a51a3
Malik Öztürk <malikozturk975@gmail.com>
2025-03-06 23:28:40 +0300

    feat: change homepage components

--- src/app/saat-kac/page.tsx
+++ src/app/saat-kac/page.tsx
@@ -10,12 +10,12 @@ export const metadata = async () => {
     return await generateMetadata({ params: { slug: 'saat-kac' } })
 }
 
-export default async function Home() {
+export default async function WhatTime() {
     try {
         const [getCitiesTimezones, getCountriesTimezones, getTurkeyData]: [TimezoneData[], TimezoneData[], TimeData] = await Promise.all([
             getRequest("/timezones/city?limit=45"),
             getRequest("/timezones/country?limit=45"),
-            getRequest("/timezones/istanbul"),
+            getRequest("/timezones/turkiye"),
         ]);
         return (
             <div className="flex flex-col items-center gap-5 md:gap-10">

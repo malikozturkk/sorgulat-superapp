1bab306e4329ca863cf44b3a4bf1cf4668e07818
malikozturkk <malikozturk975@gmail.com>
2025-03-10 17:12:55 +0300

    fix: remove

--- src/components/Timezone/SearchForm/index.tsx
+++ src/components/Timezone/SearchForm/index.tsx
@@ -75,11 +75,7 @@ export default function SearchForm() {
                     type="text"
                     value={query}
                     onChange={handleChange}
-                    onClick={(e) => {
-                        e.preventDefault();
-                        setShowAutoComplete(true);
-                    }}
-                    onTouchStart={(e) => e.preventDefault()}
+                    onClick={() => setShowAutoComplete(true)}
                 />
                 <button onClick={() => setShowInput(false)}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="#646ecb" viewBox="0 0 24 24" role="none"><path d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" role="none"></path></svg>
@@ -139,11 +135,7 @@ export default function SearchForm() {
                     type="text"
                     value={query}
                     onChange={handleChange}
-                    onClick={(e) => {
-                        e.preventDefault();
-                        setShowAutoComplete(true);
-                    }}
-                    onTouchStart={(e) => e.preventDefault()}
+                    onClick={() => setShowAutoComplete(true)}
                 />
                 {result?.length > 0 && showAutoComplete ? (
                     <div ref={suggestionRef} className="absolute top-12 left-0 bg-white text-black rounded-md shadow-md w-96 z-50" style={{ opacity: ".95" }}>

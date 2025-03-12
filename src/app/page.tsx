4f5355aaaea965cdefc5eca7ae868bb49ae03b17
malikozturkk <malikozturk975@gmail.com>
2025-03-12 12:30:44 +0300

    feat: add user location for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { TimeData, TimezoneData } from "./saat-kac/types/Timezone.types";
 import LiveClock from "@/components/Timezone/LiveClock";
 import RandomItems from "@/components/Timezone/RandomItems";
 import { generateMetadata } from "./layout";
+import { headers } from 'next/headers'
+import { getUserLocation } from "./saat-kac/page";
 
 export const dynamic = "force-dynamic";
 
@@ -11,11 +13,14 @@ export const metadata = async () => {
 }
 
 export default async function Home() {
+  const headersList = await headers()
+  const ip = headersList.get("x-forwarded-for") || "auto";
+  const userLocation = await getUserLocation(ip)
   try {
     const [getCitiesTimezones, getCountriesTimezones, getUserData]: [TimezoneData[], TimezoneData[], TimeData] = await Promise.all([
       getRequest("/timezones/city?limit=45"),
       getRequest("/timezones/country?limit=45"),
-      getRequest("/timezones/turkiye"),
+      getRequest(`/timezones/${encodeURIComponent(userLocation.toLowerCase())}`),
     ]);
     return (
       <div className="flex flex-col items-center gap-5 md:gap-10">
--- src/app/saat-kac/page.tsx
+++ src/app/saat-kac/page.tsx
@@ -11,7 +11,7 @@ export const metadata = async () => {
     return await generateMetadata({ params: { slug: 'saat-kac' } })
 }
 
-async function getUserLocation(ip: string) {
+export async function getUserLocation(ip: string) {
     try {
         const res = await fetch(`http://ip-api.com/json/${ip}?fields=country,city`);
         const data = await res.json();

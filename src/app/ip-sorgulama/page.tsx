f207e39ead686fb1559ed03b7ac0eedc7f87cf59
malikozturkk <malikozturk975@gmail.com>
2025-03-12 12:41:28 +0300

    fix: change get ip method

--- src/app/ip-sorgulama/page.tsx
+++ src/app/ip-sorgulama/page.tsx
@@ -1,5 +1,6 @@
 import IpForm from '@/components/IpForm';
 import { generateMetadata } from '../layout';
+import { headers } from 'next/headers'
 
 export const metadata = async () => {
     return await generateMetadata({ params: { slug: 'ip-sorgulama' } });
@@ -7,11 +8,13 @@ export const metadata = async () => {
 
 export default async function IpInquiry() {
     try {
-        const res = await fetch("https://ipapi.co/json/");
+        const headersList = await headers()
+        const ip = headersList.get("x-forwarded-for") || "auto";
+        const res = await fetch(`https://ipapi.co/${ip}/json/`);
         const data = await res.json();
         return (
             <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8 flex flex-col gap-1 pb-6 md:pb-12 md:gap-2">
-                <IpForm firstIp={data.ip} firstIpData={data} />
+                <IpForm firstIp={ip} firstIpData={data} />
             </div>
         );
     } catch (error) {

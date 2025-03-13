64edb2ab2373c65311e51b6da9c8119db83c52e6
malikozturkk <malikozturk975@gmail.com>
2025-03-13 18:01:24 +0300

    fix: change requests for saat kac page

--- src/app/saat-kac/page.tsx
+++ src/app/saat-kac/page.tsx
@@ -25,11 +25,13 @@ export default async function WhatTime() {
     const headersList = await headers()
     const ip = headersList.get("x-forwarded-for") || "auto";
     const userLocation = await getUserLocation(ip)
-    try {
-        const [getCitiesTimezones, getCountriesTimezones, getUserData]: [TimezoneData[], TimezoneData[], TimeData] = await Promise.all([
-            getRequest("/timezones/city?limit=45"),
-            getRequest("/timezones/country?limit=45"),
-            getRequest(`/timezones/${encodeURIComponent(userLocation.toLowerCase())}`),
+    const [getCitiesTimezones, getCountriesTimezones, getUserData]: [TimezoneData[], TimezoneData[], TimeData] =
+        await Promise.all([
+        getRequest("/timezones/city?limit=45"),
+        getRequest("/timezones/country?limit=45"),
+        getRequest(`/timezones/${encodeURIComponent(userLocation)}`).catch(() =>
+        getRequest("/timezones/turkiye")
+        ),
         ]);
         return (
             <div className="flex flex-col items-center gap-5 md:gap-10">
@@ -37,7 +39,4 @@ export default async function WhatTime() {
                 <RandomItems getCitiesTimezones={getCitiesTimezones} getCountriesTimezones={getCountriesTimezones} />
             </div>
         );
-    } catch (e) {
-        return `Error -> ${e}`
-    }
 }

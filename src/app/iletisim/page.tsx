53c2e582d5d9de688e8de1492471088b9e97655a
Malik <malikozturk975@gmail.com>
2025-04-04 18:29:57 +0300

    feat: change contact page

--- src/app/iletisim/page.tsx
+++ src/app/iletisim/page.tsx
@@ -1,5 +1,8 @@
 import Link from 'next/link';
 import { generateMetadata } from '../layout';
+import { TbArticle, TbClock, TbFlag, TbMail, TbSocial, TbWorld } from "react-icons/tb";
+import { FaXTwitter } from 'react-icons/fa6';
+import { FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 export const metadata = async () => {
     return await generateMetadata({ params: { slug: 'iletisim' } });
@@ -8,47 +11,89 @@ export const metadata = async () => {
 export default async function Contact() {
     return (
         <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8 flex flex-col gap-1 pb-6 md:pb-12 md:gap-2">
-            <h1 className='text-2xl md:text-4xl font-extrabold mb-4 md:mb-6'>
-                <Link className='text-primary font-bold hover:opacity-80 duration-150' href="mailto:info@sorgulat.com">
-                    Sorgulat.com
-                </Link>{' '}ile iletişime geçin
-            </h1>
-            <p className="text-sm md:text-lg leading-relaxed">
-                <Link className='text-primary font-bold hover:opacity-80 duration-150' href="/">
-                    Sorgulat.com
-                </Link>{' '}hakkında düşüncelerinizi bizimle paylaşın! Platformumuzla ilgili önerileriniz, şikayetleriniz veya sorularınız varsa bizimle iletişime geçmekten çekinmeyin.
+            <h1 className='text-2xl md:text-4xl font-extrabold mb-2 md:mb-3'>Bize Ulaşın</h1>
+            <p className="text-sm md:text-lg leading-relaxed mb-2 md:mb-3">
+                <Link className="text-primary font-bold hover:opacity-80 duration-150" href="/">Sorgulat.com</Link> hakkında düşüncelerinizi bizimle paylaşın! Platformumuzla ilgili önerileriniz, şikayetleriniz veya sorularınız varsa bizimle iletişime geçmekten çekinmeyin.
             </p>
-            <div className="bg-gray-100 p-4 rounded-lg md:p-6">
-                <h2 className="text-lg md:text-2xl font-bold mb-2">İletişim Seçenekleri</h2>
-                <ul className="text-sm md:text-lg list-disc pl-5 space-y-2">
-                    <li>
-                        E-posta: <Link className='text-primary font-bold hover:opacity-80 duration-150' href="mailto:info@sorgulat.com">info@sorgulat.com</Link>
-                    </li>
-                    <li>
-                        Web Sitesi: <Link className='text-primary font-bold hover:opacity-80 duration-150' href="/">sorgulat.com</Link>
-                    </li>
-                    <li className='list-none flex items-center gap-2'>
-                        <h3>Sosyal Medya:</h3>
-                        <a
-                        target="_blank"
-                        title="Sorgulat - Instagram"
-                        className="flex items-center justify-center text-white transition-all duration-200 bg-[#1f2937] rounded-full w-7 h-7 hover:bg-red focus:bg-red"
-                        href="https://www.instagram.com/sorgulatcom/"
-                        rel="noopener noreferrer"
-                        >
-                        <svg
-                            className="w-4 h-4"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="currentColor"
-                        >
-                            <path d="M11.999 7.377a4.623 4.623 0 1 0 0 9.248 4.623 4.623 0 0 0 0-9.248zm0 7.627a3.004 3.004 0 1 1 0-6.008 3.004 3.004 0 0 1 0 6.008z"></path>
-                            <circle cx="16.806" cy="7.207" r="1.078"></circle>
-                            <path d="M20.533 6.111A4.605 4.605 0 0 0 17.9 3.479a6.606 6.606 0 0 0-2.186-.42c-.963-.042-1.268-.054-3.71-.054s-2.755 0-3.71.054a6.554 6.554 0 0 0-2.184.42 4.6 4.6 0 0 0-2.633 2.632 6.585 6.585 0 0 0-.419 2.186c-.043.962-.056 1.267-.056 3.71 0 2.442 0 2.753.056 3.71.015.748.156 1.486.419 2.187a4.61 4.61 0 0 0 2.634 2.632 6.584 6.584 0 0 0 2.185.45c.963.042 1.268.055 3.71.055s2.755 0 3.71-.055a6.615 6.615 0 0 0 2.186-.419 4.613 4.613 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.186.043-.962.056-1.267.056-3.71s0-2.753-.056-3.71a6.581 6.581 0 0 0-.421-2.217zm-1.218 9.532a5.043 5.043 0 0 1-.311 1.688 2.987 2.987 0 0 1-1.712 1.711 4.985 4.985 0 0 1-1.67.311c-.95.044-1.218.055-3.654.055-2.438 0-2.687 0-3.655-.055a4.96 4.96 0 0 1-1.669-.311 2.985 2.985 0 0 1-1.719-1.711 5.08 5.08 0 0 1-.311-1.669c-.043-.95-.053-1.218-.053-3.654 0-2.437 0-2.686.053-3.655a5.038 5.038 0 0 1 .311-1.687c.305-.789.93-1.41 1.719-1.712a5.01 5.01 0 0 1 1.669-.311c.951-.043 1.218-.055 3.655-.055s2.687 0 3.654.055a4.96 4.96 0 0 1 1.67.311 2.991 2.991 0 0 1 1.712 1.712 5.08 5.08 0 0 1 .311 1.669c.043.951.054 1.218.054 3.655 0 2.436 0 2.698-.043 3.654h-.011z"></path>
-                        </svg>
-                        </a>
-                    </li>
-                </ul>
+            <div className='flex items-start gap-6 w-full flex-col md:flex-row'>
+                <div className='w-full md:w-1/2 flex flex-col gap-6'>
+                    <div className='border border-gray-100 bg-white p-4 rounded-lg w-full shadow-md'>
+                        <div className='flex flex-col gap-4'>
+                            <h2 className='text-lg font-semibold'>İletişim Seçenekleri</h2>
+                            <div className='flex items-center gap-4'>
+                                <TbMail className='min-w-6 w-6 h-6 text-primary' />
+                                <Link className="font-bold hover:opacity-80 duration-150 hover:text-primaryDark" href="mailto:info@sorgulat.com">info@sorgulat.com</Link>
+                            </div>
+                            <div className='flex items-center gap-4'>
+                                <TbWorld className='min-w-6 w-6 h-6 text-primary' />
+                                <Link className="font-bold hover:opacity-80 duration-150 hover:text-primaryDark" href="/" target='_blank'>sorgulat.com</Link>
+                            </div>
+                            <div className="flex items-center gap-4">
+                                <TbSocial className='min-w-6 w-6 h-6 text-primary' />
+                                <Link
+                                    target="_blank"
+                                    title="Sorgulat - Instagram"
+                                    className="flex items-center justify-center"
+                                    href="https://www.instagram.com/sorgulatcom/"
+                                    rel="noopener noreferrer"
+                                    aria-label="Sorgulat Instagram"
+                                    >
+                                    <FaInstagram className="text-pink-500 w-5 h-5" />
+                                </Link>
+                                <Link
+                                    target="_blank"
+                                    title="Sorgulat - Linkedin"
+                                    className="flex items-center justify-center"
+                                    href="https://www.linkedin.com/company/sorgulat/"
+                                    rel="noopener noreferrer"
+                                    aria-label="Sorgulat Linkedin"
+                                    >
+                                    <FaLinkedin className="text-blue-600 w-5 h-5" />
+                                </Link>
+                                <Link
+                                    target="_blank"
+                                    title="Sorgulat - X"
+                                    className="flex items-center justify-center"
+                                    href="https://x.com/sorgulatcom"
+                                    rel="noopener noreferrer"
+                                    aria-label="Sorgulat X"
+                                    >
+                                    <FaXTwitter className="w-5 h-5" />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='border border-gray-100 bg-white p-4 rounded-lg w-full shadow-md'>
+                        <div className='flex flex-col gap-4'>
+                            <h2 className='text-lg font-semibold'>Sorgulat.com Hakkında</h2>
+                            <div className='flex gap-4'>
+                                <TbClock className='min-w-6 w-6 h-6 text-primary' />
+                                <p>Dünyadaki şehirler için güncel saati ve saat farklarını kontrol edin.</p>
+                            </div>
+                            <div className='flex gap-4'>
+                                <TbFlag className='min-w-6 w-6 h-6 text-primary' />
+                                <p>Türk pasaportu için ülkelerin vize gereksinimlerini görün.</p>
+                            </div>
+                            <div className='flex gap-4'>
+                                <TbArticle className='min-w-6 w-6 h-6 text-primary' />
+                                <p>Blogumuzda seyahat ve diğer konularla ilgili yazıları keşfedin.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='border border-gray-100 bg-white p-4 rounded-lg w-full md:w-1/2 shadow-md'>
+                    <div className='flex flex-col gap-4'>
+                        <h2 className='text-lg font-semibold'>İletişim Formu</h2>
+                        <form className='flex flex-col gap-4'>
+                            <input type='text' name='name' placeholder="İsim Soyisim" className='p-3 border border-gray-200 rounded-lg' />
+                            <input type='email' name='email' placeholder="Email" className='p-3 border border-gray-200 rounded-lg' />
+                            <textarea name='message' placeholder="Mesaj" className='p-3 border border-gray-200 rounded-lg h-28' />
+                            <button className="bg-primary text-white px-4 py-2 rounded h-12 w-full">
+                                Gönder
+                            </button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     );

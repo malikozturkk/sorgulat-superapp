0e271299e4581ade183fa63de6b8771aa9e6b13e
Malik Öztürk <malikozturk975@gmail.com>
2025-07-03 10:01:38 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,17 +16,9 @@ export default function RootLayout({
       <head>
       <AdSense pId="4912331234184158" />
       <link
-        rel="preload"
-        href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700&display=swap"
-        as="style"
-        // @ts-ignore
-        onLoad={(e) => { e.currentTarget.rel = 'stylesheet'; }}
-      />
-      <noscript
-        dangerouslySetInnerHTML={{
-          __html: `<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700&display=swap" />`,
-        }}
-      />
+          href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       <Script
         strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=G-1VTVJYYXQR"
